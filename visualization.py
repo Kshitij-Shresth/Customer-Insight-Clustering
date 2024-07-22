fc5eed978b2c786ebe6a55c7d82b451fd5856ee0
Kshitij Shresth <kshitijshresth@icloud.com>
2024-07-22 08:28:42 +0530

    Update visualization.py
    3D scatter plot of customer segments based on purchase frequency, total spend, and average purchase value.

--- visualization.py
+++ visualization.py
@@ -5,3 +5,19 @@ def plot_elbow(sse):
     plt.ylabel('SSE')
     plt.title('Elbow Method')
     plt.show()
+
+def plot_clusters(customer_data):
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(111, projection='3d')
+    scatter = ax.scatter(customer_data['InvoiceNo'],
+                         customer_data['TotalSpend'],
+                         customer_data['AveragePurchaseValue'],
+                         c=customer_data['Cluster'],
+                         cmap='viridis')
+    ax.set_xlabel('Purchase Frequency')
+    ax.set_ylabel('Total Spend')
+    ax.set_zlabel('Average Purchase Value')
+    ax.set_title('Customer Segments')
+    legend1 = ax.legend(*scatter.legend_elements(), title="Clusters")
+    ax.add_artist(legend1)
+    plt.show()
